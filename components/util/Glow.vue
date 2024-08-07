bde959b8c78faa9c9acecd5d5ccf7ff9108029e8
ArtusLama <artuslama@gmail.com>
2024-08-07 22:05:28 +0200

    :art: Improved Glow Component

---
+++ components/util/Glow.vue
@@ -0,0 +1,20 @@
+<template>
+    
+    <span class="group">
+        <span class="absolute blur-[10px] pointer-events-none select-none transition-opacity" :class="onHover ? 'opacity-25 group-hover:opacity-75' : 'opacity-75'">
+            <slot />
+        </span>
+        <slot />
+    </span>
+
+</template>
+<script setup lang="ts">
+
+    defineProps({
+        onHover: {
+            type: Boolean,
+            default: false
+        }
+    });
+
+</script>
--- components/util/GlowText.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    
-    <slot/>
-    <span class="absolute translate-x-[-100%] blur-[10px] opacity-75 pointer-events-none select-none">
-        <slot/>
-    </span>
-    
-</template>
