ab562fe2234989c551e3a613225381ea4068b964
ArtusLama <artuslama@gmail.com>
2024-08-07 22:02:48 +0200

    :art: Add Badge Component

---
+++ components/util/Badge.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="select-none font-light rounded-full bg-background border border-borderLight hover:bg-lightBackground transition-colors px-5 py-2 flex items-center justify-center gap-3">
+
+        <slot />
+        
+    </div>
+</template>
