1c47512e8328bec801d396716013351210f4fafd
ArtusLama <artuslama@gmail.com>
2024-08-07 22:18:30 +0200

    :construction: Work in Progress Section

--- components/util/WorkInProgress.vue
+++ components/util/WorkInProgress.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full flex flex-col items-center justify-center gap-4">
+    <div id="workinprogress-sec" class="w-full flex flex-col items-center justify-center gap-4 h-screen">
 
         <div class="flex items-center gap-4">
             <img class="w-10" src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Robot.png" alt="Robot" width="25" height="25" />
@@ -8,11 +8,15 @@
         </div>
 
         <div class="w-1/3 flex flex-col items-center">
-            <p class="text-center text-normalText"><UtilGlowText>This page isn't availible yet!</UtilGlowText><br>If you have any questions or just want to contact me don't hesitate:</p>
+            <p class="text-center text-normalText"><UtilGlow>This page isn't availible yet!</UtilGlow><br>If you have any questions or just want to contact me don't hesitate:</p>
             
-            <div class="mt-2 flex gap-10 items-center justify-center">
-                <p class="flex items-center justify-center gap-2"><Icon name="bxl:discord-alt" class="size-7 mt-[1.5px]"/>artus_dev</p>
-                <a class="flex items-center justify-center gap-2" href="https://github.com/ArtusLama" target="_blank"><Icon name="bxl:github" class="size-7"/>ArtusLama</a>
+            <div class="mt-8 flex gap-10 items-center justify-center">
+                <UiButton>
+                    <p class="flex items-center justify-center gap-2"><Icon name="bxl:discord-alt" class="size-7 mt-[1.5px]"/>artus_dev</p>
+                </UiButton>
+                <UiButton>
+                    <a class="flex items-center justify-center gap-2" href="https://github.com/ArtusLama" target="_blank"><Icon name="bxl:github" class="size-7"/>ArtusLama</a>
+                </UiButton>
             </div>
         </div>
 
