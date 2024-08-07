c4d1005a656beb9507497430c4941c73001adbc0
ArtusLama <artuslama@gmail.com>
2024-08-07 22:07:52 +0200

    :lipstick: Prettier Primary Gradient

--- components/util/PrimaryTextGradient.vue
+++ components/util/PrimaryTextGradient.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <UtilTextGradient class="bg-gradient-to-b from-white to-primary">
+    <UtilTextGradient class="bg-gradient-to-b from-white to-primary to-80%">
         <slot/>
     </UtilTextGradient>
 
