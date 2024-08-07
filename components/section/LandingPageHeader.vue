d5d5b67d0d89d8336dfd927f7dc8f3f21aa8bcf0
ArtusLama <artuslama@gmail.com>
2024-08-07 22:23:52 +0200

    :lipstick: Add Hire Badge + Better down arrow

--- components/section/LandingPageHeader.vue
+++ components/section/LandingPageHeader.vue
@@ -1,19 +1,30 @@
 <template>
-    <div class="h-screen flex flex-col items-center *:z-10">
-        
-        <div class="flex flex-col items-center mt-[35vh]">
-            <div class="text-5xl text-lightText font-bold tracking-[-1px] text-center">
-                <p>Driven by <UtilPrimaryTextGradient>passion</UtilPrimaryTextGradient><span class="absolute -mt-12 -ml-4"><LottieSparkles /></span></p>
-                <p>I craft stunning <UtilGlowText>designs</UtilGlowText> and reliable <UtilGlowText>code</UtilGlowText>.</p>
-            </div>            
+    <div class="flex flex-col items-center min-h-screen">
+
+        <div class="flex flex-col items-center pt-[30vh]" id="sec1">
+            <UtilBadge>
+                <div class="relative">
+                    <div class="absolute animate-[ping_1500ms_ease-out_infinite] inset-0 bg-primary/85 w-3 aspect-square rounded-full"></div>
+                    <UtilGlow><div class="bg-primary/85 w-3 aspect-square rounded-full"></div></UtilGlow>
+                </div>
+                <div class="-mt-[1px]"><UtilGlow on-hover>Available for hiring</UtilGlow></div>
+            </UtilBadge>
+
+            <div class="mt-4 text-5xl text-lightText font-bold tracking-[-1px] text-center">
+                <p>Driven by <UtilPrimaryTextGradient>passion</UtilPrimaryTextGradient><span class="absolute -mt-12 -ml-4"><LottieSparkles/></span></p>
+                <p>I craft stunning <UtilGlow>designs</UtilGlow> and reliable <UtilGlow>code</UtilGlow>.</p>
+            </div>
 
             <div class="mt-10 flex gap-10">
                 <UiButton><Icon name="solar:user-outline" size="20"/>About Me</UiButton>
                 <UiButton primary><Icon name="solar:case-outline" size="20"/>My Work</UiButton>
             </div>
 
-            <Icon name="solar:alt-arrow-down-outline" size="35" class="text-lightText animate-bounce absolute bottom-10"/>
+
+            <UiScrollArrow scroll-to-id="workinprogress-sec" class="absolute bottom-10" />
+            
         </div>
+
         
     </div>
 </template>
